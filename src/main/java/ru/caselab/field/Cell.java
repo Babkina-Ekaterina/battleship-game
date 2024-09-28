93cf4b6dc5d78ac532cebc6657f66d25f2009aa0
Babkina Ekaterina <bkatya5577@yandex.ru>
2024-09-28 21:22:49 +0300

    Add cell

---
+++ src/main/java/ru/caselab/field/Cell.java
@@ -0,0 +1,23 @@
+package ru.caselab.field;
+
+import ru.caselab.state.CellState;
+
+public class Cell {
+    private CellState cellState = CellState.EMPTY;
+    private Ship ship;
+
+    public CellState getCellState() {
+        return cellState;
+    }
+    public void setCellState(CellState cellState) {
+        this.cellState = cellState;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+}
